bb893b52565354f4108265c23bfb2920eb07174c
ALitch0 <123614243+ALitch0@users.noreply.github.com>
2023-03-06 09:11:57 -0500

    Question 3

--- shows.php
+++ shows.php
@@ -10,6 +10,39 @@
     <?php 
     //capturing the value using $_POST array and storing into a variable
     $name = $_POST['name'];
+    //connecting to database
+    $db = new PDO('mysql:host=172.31.22.43;dbname=Alish200535161','Alish200535161','wXXqdNueNA');
+    //creating sql insert command
+    $sql = "SELECT * FROM services WHERE name = :name;";
+    //preparing and binding sql insert
+    $cmd=$db->prepare($sql);
+    $cmd->bindParam(':name',$name,PDO::PARAM_STR);
+    $cmd->execute();
+    //fetching data to service variable
+    $service= $cmd->fetch();
+    
+    //using fetched data to get serviceId
+    $serviceId = $service['serviceId'];
+    
+    //setting up new sql insert for shows table
+    $sql = "SELECT * FROM shows WHERE serviceId = :serviceId;";
+    
+    //preparing and binding sql insert
+    $cmd=$db->prepare($sql);
+    $cmd->bindParam(':serviceId',$serviceId,PDO::PARAM_INT);
+    $cmd->execute();
+    
+    //fetching all data into shows
+    $shows=$cmd->fetchAll();
+
+    //creating a list for the selected services shows
+    echo '<h1>Shows for '.$name.'</h1>';
+    echo '<ol>';
+    foreach($shows as $show){
+        echo '
+        <li>'.$show['title'].'</li>
+        ';
+    }
     ?>
 </body>
 </html>
